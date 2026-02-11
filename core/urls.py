df5a4a1c60af4dbf3254114458ba52191d52eeed
EKA SYARIF MAULANA <diann8565@gmail.com>
2026-02-11 10:28:19 +0700

    Add root view to fix 404

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Django Backend is Running", "status": "ok"})
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
